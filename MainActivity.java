e5528c15986f762ea1f46d29508ca498471150a8
NadenkaTropanova <73659062+NadenkaTropanova@users.noreply.github.com>
2021-02-17 16:41:57 +0300

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.mycounter;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private Integer counter = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @SuppressLint("SetTextI18n")
+    public void onClickBtnAddStudents(View view) {
+        counter++;
+        TextView counterView = findViewById(R.id.txt_counter);
+        counterView.setText(counter.toString());
+    }
+}
